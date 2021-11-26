ba1402426a3f74dbeca85ed24891b38ab589aa67
E-lir <34363661+E-lir@users.noreply.github.com>
2021-11-27 10:15:12 +1300

    Add files via upload

--- CryptoTradingBot/main.py
+++ CryptoTradingBot/main.py
@@ -2,10 +2,11 @@ from binance import Client
 import pandas as pd
 import websockets
 import json
-import asyncio
 import requests
 import ta
-
+import asyncio
+from binance import AsyncClient, BinanceSocketManager
+from MoneyManager import MoneyManager
 
 with open('keys.txt') as f:
     api_key = f.readline().strip()
@@ -14,112 +15,161 @@ with open('keys.txt') as f:
 
 stream = websockets.connect('wss://stream.binance.com:9443/stream?streams=ethusdt@miniTicker')
 
-state = 'ready'
-entry_price = 0
-bal_usd = 100
-bal_eth = 0
-
 
 async def main():
-    df = pd.DataFrame()
     short_frame = pd.DataFrame()
+    money_manager = MoneyManager('ready', 0, 0, 100)
 
-    # get historical data
+    # get 5 min historical data
     url = 'https://api.binance.com/api/v3/klines'
-    params = {'symbol': 'ETHUSDT', 'interval': '15m'}
-    df = pd.DataFrame(json.loads(requests.get(url, params=params).text))
-    df = df.iloc[:, [0, 4]]
-    df.columns = ['Time', 'Price']
-    df.Time = pd.to_datetime(df.Time, unit='ms')
+    params = {'symbol': 'ETHUSDT', 'interval': '5m', 'limit': 50}
+    df5 = pd.DataFrame(json.loads(requests.get(url, params=params).text))
+    df5 = df5.iloc[:, [0, 4]]
+    df5.columns = ['Time', 'Price']
+    df5.Time = pd.to_datetime(df5.Time, unit='ms')
 
-    newest_time = df.iloc[-1].Time
+    # get 15 min historical data
+    params = {'symbol': 'ETHUSDT', 'interval': '15m', 'limit': 50}
+    df15 = pd.DataFrame(json.loads(requests.get(url, params=params).text))
+    df15 = df15.iloc[:, [0, 4]]
+    df15.columns = ['Time', 'Price']
+    df15.Time = pd.to_datetime(df15.Time, unit='ms')
 
-    # get new data
-    while True:
-        async with stream as receiver:
-            data = await receiver.recv()
-            data = json.loads(data)['data']
-            row = create_frame(data)
-            short_frame = short_frame.append(row)
+    timer5m = df5.iloc[-1].Time
+    timer15m = df15.iloc[-1].Time
+    short_timer = df5.iloc[-1].Time
 
-            # short_frame is for short term price data, 100 rows ~ 16.5 minutes worth
-            if len(short_frame) > 100:
-                short_frame = short_frame.iloc[1:, :]
+    cl = await AsyncClient.create()
+    bm = BinanceSocketManager(cl)
+    # start any sockets here, i.e a trade socket
+    ts = bm.trade_socket('ETHUSDT')
+    # then start receiving messages
+    async with ts as tscm:
+        while True:
+            res = await tscm.recv()
+            row = create_frame(res)
+            # update 1 sec data frame
+            if row.iloc[-1].Time >= short_timer + pd.Timedelta(seconds=1):
+                short_frame = short_frame.append(row)
+                short_timer = short_timer + pd.Timedelta(seconds=1)
+                # short_frame is for short term price data, keep most recent 300 rows - 5 minutes worth
+                if len(short_frame) > 300:
+                    short_frame = short_frame.iloc[1:, :]
 
-            # append new row to df every 15 m
-            if row.iloc[-1].Time > newest_time + pd.Timedelta(minutes=15):
-                df = df.append(row)
-                newest_time = newest_time + pd.Timedelta(minutes=15)
+                strategy(money_manager, df5, df15, short_frame)
 
-            # implement strategy
-            strategy(df, short_frame)
+            # update 5 min data frame
+            if row.iloc[-1].Time > timer5m + pd.Timedelta(minutes=5):
+                df5 = df5.append(row)
+                timer5m = timer5m + pd.Timedelta(minutes=5)
+                # keep most recent 100 rows
+                if len(df5) > 100:
+                    df5 = df5.iloc[1:, :]
+
+            # update 15 min data frame
+            if row.iloc[-1].Time > timer15m + pd.Timedelta(minutes=15):
+                df15 = df15.append(row)
+                timer15m = timer15m + pd.Timedelta(minutes=15)
+                # keep most recent 100 rows
+                if len(df15) > 100:
+                    df15 = df15.iloc[1:, :]
+
+    await client.close_connection()
 
 
-def strategy(df, short_frame):
-    global state
-    global entry_price
-    global bal_eth
-    global bal_usd
+def strategy(money_manager, df5, df15, short_frame):
 
-    bb = ta.volatility.BollingerBands(close=df.Price, window=20, window_dev=2, fillna=False)
+    bb = ta.volatility.BollingerBands(close=df15.Price, window=20, window_dev=2, fillna=False)
     h_band = bb.bollinger_hband().iloc[-1]
     l_band = bb.bollinger_lband().iloc[-1]
-    m_band = bb.bollinger_mavg().iloc[-1]
-    sma1 = ta.trend.SMAIndicator(close=short_frame.Price, window=6, fillna=False)
-    sma5 = ta.trend.SMAIndicator(close=short_frame.Price, window=30, fillna=False)
+    sma1 = ta.trend.SMAIndicator(close=short_frame.Price, window=10, fillna=False)
+    macd = ta.trend.MACD(close=df5.Price, window_slow=26, window_fast=12, window_sign=9, fillna=False)
 
-    if state == 'ready':
+    if money_manager.state == 'ready':
         # shorting opportunity
         if sma1.sma_indicator().iloc[-1] >= h_band:
-            state = 'shorting'
+            money_manager.state = 'shorting'
+            print(money_manager.state)
 
         # longing opportunity
         if sma1.sma_indicator().iloc[-1] <= l_band:
-            state = 'longing'
+            money_manager.state = 'longing'
 
-    elif state == 'shorting':
-        if sma5.sma_indicator().iloc[-7] > sma5.sma_indicator().iloc[-1]:
-            bal_eth = bal_usd / short_frame.Price.iloc[-1] * -1
-            bal_usd = bal_usd * 2
-            entry_price = short_frame.Price.iloc[-1]
-            state = 'short'
-            print("SHORT")
-            print("ETH Bal: " + str(bal_eth) + "    USD Bal: " + str(bal_usd) + "     Entry price: " + str(entry_price))
+    elif money_manager.state == 'shorting':
+        # make sure price hasn't already gone to other side of BB
+        if sma1.sma_indicator().iloc[-1] <= l_band:
+            money_manager.state = 'longing'
 
-    elif state == 'longing':
-        if sma5.sma_indicator().iloc[-7] < sma5.sma_indicator().iloc[-1]:
-            bal_eth = bal_usd / short_frame.Price.iloc[-1]
-            bal_usd = 0
-            entry_price = short_frame.Price.iloc[-1]
-            state = 'long'
-            print("LONG")
-            print("ETH Bal: " + str(bal_eth) + "    USD Bal: " + str(bal_usd) + "     Entry price: " + str(entry_price))
+        elif macd.macd_diff().iloc[-2] > 0 > macd.macd_diff().iloc[-1]:
+            # set stop loss at high bollinger band and profit at 1:1 ratio
+            stop_loss = (h_band - sma1.sma_indicator().iloc[-1]) / sma1.sma_indicator().iloc[-1]
+            # don't enter trade if profit not enough to cover trading fee 0.75%
+            if stop_loss <= 0.0015:
+                money_manager.state = 'ready'
 
-    elif state == 'short':
-        # wait for price to hit middle bollinger band
-        if short_frame.Price.iloc[-1] <= m_band or short_frame.Price.iloc[-1] > entry_price * 1.005:
-            bal_usd = bal_usd + (bal_eth * short_frame.Price.iloc[-1])
-            bal_eth = 0
-            state = 'ready'
-            print("CLOSED SHORT")
-            print("ETH Bal: " + str(bal_eth) + "    USD Bal: " + str(bal_usd) + "     Exit price: " + str(short_frame.Price.iloc[-1]))
+            else:
+                money_manager.bal_eth = money_manager.bal_usd / short_frame.Price.iloc[-1] * -1
+                money_manager.bal_usd = money_manager.bal_usd * 2
+                money_manager.entry_price = short_frame.Price.iloc[-1]
+                money_manager.state = 'short'
+                print("SHORT   " + str(short_frame.Time.iloc[-1]))
+                print("Entry price: " + str(money_manager.entry_price) + "    Stop Loss: " + str(stop_loss))
 
-    elif state == 'long':
-        # wait for price to hit middle bollinger band
-        if short_frame.Price.iloc[-1] >= m_band or short_frame.Price.iloc[-1] < entry_price / 1.005:
-            bal_usd = bal_eth * short_frame.Price.iloc[-1]
-            bal_eth = 0
-            state = 'ready'
-            print("CLOSED LONG")
-            print("ETH Bal: " + str(bal_eth) + "    USD Bal: " + str(bal_usd) + "     Exit price: " + str(short_frame.Price.iloc[-1]))
+    elif money_manager.state == 'longing':
+        # make sure price hasn't already gone to other side of BB
+        if sma1.sma_indicator().iloc[-1] >= h_band:
+            money_manager.state = 'shorting'
+
+        elif macd.macd_diff().iloc[-2] < 0 < macd.macd_diff().iloc[-1]:
+            # set stop loss at low bollinger band and profit at 1:1 ratio
+            stop_loss = (sma1.sma_indicator().iloc[-1] - l_band) / l_band
+            # don't enter trade if profit not enough to cover trading fee 0.75%
+            if stop_loss <= 0.0015:
+                money_manager.state = 'ready'
+
+            else:
+                money_manager.bal_eth = money_manager.bal_usd / short_frame.Price.iloc[-1]
+                money_manager.bal_usd = 0
+                money_manager.entry_price = short_frame.Price.iloc[-1]
+                money_manager.state = 'long'
+                print("LONG   " + str(short_frame.Time.iloc[-1]))
+                print("Entry price: " + str(money_manager.entry_price) + "    Stop Loss: " + str(stop_loss))
+
+    elif money_manager.state == 'short':
+        # set stop loss at high bollinger band and profit at 1:1 ratio
+        stop_loss = (h_band - money_manager.entry_price) / money_manager.entry_price
+        # limit stop loss to 0.8%
+        if stop_loss > 0.008:
+            stop_loss = 0.008
+
+        elif short_frame.Price.iloc[-1] >= money_manager.entry_price * (1 + stop_loss) or short_frame.Price.iloc[-1] <= money_manager.entry_price / (1 + stop_loss):
+            money_manager.bal_usd = money_manager.bal_usd + (money_manager.bal_eth * short_frame.Price.iloc[-1])
+            money_manager.bal_eth = 0
+            money_manager.state = 'ready'
+            print("CLOSED SHORT   " + str(short_frame.Time.iloc[-1]))
+            print("ETH Bal: " + str(money_manager.bal_eth) + "    USD Bal: " + str(money_manager.bal_usd) + "     Exit price: " + str(short_frame.Price.iloc[-1]))
+
+    elif money_manager.state == 'long':
+        # set stop loss at low bollinger band and profit at 1:1 ratio
+        stop_loss = (money_manager.entry_price - l_band) / l_band
+        # limit stop loss to 0.8%
+        if stop_loss > 0.008:
+            stop_loss = 0.008
+
+        elif short_frame.Price.iloc[-1] <= money_manager.entry_price / (1 + stop_loss) or short_frame.Price.iloc[-1] >= money_manager.entry_price * (1 + stop_loss):
+            money_manager.bal_usd = money_manager.bal_eth * short_frame.Price.iloc[-1]
+            money_manager.bal_eth = 0
+            money_manager.state = 'ready'
+            print("CLOSED LONG   " + str(short_frame.Time.iloc[-1]))
+            print("ETH Bal: " + str(money_manager.bal_eth) + "    USD Bal: " + str(money_manager.bal_usd) + "     Exit price: " + str(short_frame.Price.iloc[-1]))
 
     else:
-        print("Invalid State: " + state)
+        print("Invalid State: " + money_manager.state)
 
 
 def create_frame(msg):
     df = pd.DataFrame([msg])
-    df = df.loc[:, ['E', 'c']]
+    df = df.loc[:, ['E', 'p']]
     df.columns = ['Time', 'Price']
     df.Price = df.Price.astype(float)
     df.Time = pd.to_datetime(df.Time, unit='ms')
